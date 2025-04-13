71c9ed34c0dc52967f78144b4995d2c5e19f1196
mygo5 <aidanvega13@gmail.com>
2025-04-13 12:55:42 -0400

    Merged with obstacles

--- utils.py
+++ utils.py
@@ -50,7 +50,7 @@ AGENT_TRAIT_PROFILES = {
         "sensing_radius": 0.2, # meters - Example values - ADJUST AS NEEDED
         "max_speed": 0.18, # m/s
         "initial_pheromone_strength": 1.0, # Initial pheromone strength for agents of this type
-        "communication_radius": 0.85, # meters
+        "communication_radius": 0.50, # meters
         "pheromone_lifetime": 150.0, # Decay rate per timestep
     },
     "Profile_Type_B": { # Example profile 2
